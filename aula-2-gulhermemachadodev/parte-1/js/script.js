ec62bcd74793bd69e16739a0d09fbaddd6b6691a
Guilherme Machado <guisilvam19@gmail.com>
2024-12-16 17:50:35 -0300

    Alterei o numero da aula

---
+++ aula-2-gulhermemachadodev/parte-1/js/script.js
@@ -0,0 +1,8 @@
+        // Mostra uma caixa de alerta simples com a mensagem "Hello World!"
+        window.alert("Hello World!");
+
+        // Exibe uma caixa de confirmação com a mensagem e botões "OK" e "Cancelar"
+        window.confirm("Bem vindo, ao Guilherme Machado Dev. Deseja Continuar?");
+
+        // Exibe uma caixa de prompt para o usuário digitar uma resposta
+        window.prompt("Qual é a sua linguagem de programação favorita?");
---
+++ aula-2-gulhermemachadodev/parte-2/js/script.js
@@ -0,0 +1,8 @@
+console.log("Guilherme Machado DEV") // Imprime o nome e título no console
+
+/* 
+ Este é um comentário 
+ de múltiplas linhas 
+*/
+
+// este é um comentário de uma linha 
